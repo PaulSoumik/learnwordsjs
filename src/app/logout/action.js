39065eb90368d2178f52ef59aa8468c706bffd65
PaulSoumik <soumikpaul1999@gmail.com>
2024-06-09 01:49:14 +0530

    R-07-cookies deletion

--- src/app/logout/action.js
+++ src/app/logout/action.js
@@ -2,12 +2,18 @@
 
 import { redirect } from 'next/navigation'
 import { signOutUser, signUpUser } from '../../../service/logicService/UserAuthenticationController';
+import { cookies } from 'next/headers';
 
 export async function handleSignOut(email){
    if(email == null) return false;
     try{
         let user = {email: email};
         let result = await signOutUser(user);
+        if(result){
+            cookies().delete('sessionId');
+            cookies().delete('useremail');
+            cookies().delete('username');
+        }
         return result;
     }catch(error){
         console.log(error)
